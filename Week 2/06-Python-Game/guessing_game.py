35a44fd241175aa2f1367689eb1362a0434075e8
Mark Hood <mark@MacBook-Air.local>
2023-01-10 20:15:13 -0600

    added Week 4 guessing game

---
+++ Week 2/06-Python-Game/guessing_game.py
@@ -0,0 +1,48 @@
+from random import randrange
+import time
+
+
+secret = randrange(0, 10, 1)
+trys = 3
+rules = '''
+#########################################################
+# This game is called "Guess the Random Number"         #
+# You get 3 tries to guess the random number between    #
+# 0 and 10.                                            #  
+#########################################################
+'''
+
+print(rules)
+print(secret)
+
+while True:
+    play = input("Do you want to play (Yes/No)? " )
+    if play.lower() == "yes" or play.lower() == "no":
+        break
+    else:
+        print("Did not enter 'Yes' or 'No'! Try again!!!")
+
+if play.lower() == "no":
+    print("Good Bye!")
+    exit()
+
+print("A random number has been generated")
+print("Can you guess it in 3 tries? \n")
+
+for t in range(0, trys):
+    print(t)
+    guess = int(input(f"Attempt #{t+1}, What is your choice? "))
+    time.sleep(1)
+    print("hmmm... let me see")
+    time.sleep(2)
+
+    if guess == secret:
+        print("Wow!!! You got it right! Congrats!")
+        quit()
+    print("Nope that was not the number")
+
+print("Sorry you did not guess the correct number... :(")
+print(f"The correct answer was {secret}. Better luck next time.")
+
+
+
