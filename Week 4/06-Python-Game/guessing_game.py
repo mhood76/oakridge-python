b7fd580e6caedc74479c28950efc7f8096af4240
mhood76 <mark.hood.ee@gmail.com>
2023-03-10 20:11:45 -0600

    Added a Week 4 folder

--- Week 4/06-Python-Game/guessing_game.py
+++ Week 4/06-Python-Game/guessing_game.py
@@ -1,29 +1,33 @@
 from random import randrange
 import time
 
+def quit_game():
+    print("Good Bye!")
+    exit()
+
 
 secret = randrange(0, 10, 1)
 trys = 3
 rules = '''
 #########################################################
-# This game is called "Guess the Random Number"         #
 # You get 3 tries to guess the random number between    #
-# 0 and 10.                                            #  
+# 0 and 10.                                             #  
 #########################################################
+
 '''
 
-print(rules)
+
 
 while True:
-    play = input("Do you want to play (Yes/No)? " )
+    play = input('This game is called "Guess the Random Number." Do you want to play (Yes/No)? ' )
     if play.lower() == "yes" or play.lower() == "no":
+        print(rules)
         break
     else:
-        print("Did not enter 'Yes' or 'No'! Try again!!!")
+        print("You did not enter 'Yes' or 'No'! Try again!!!")
 
 if play.lower() == "no":
-    print("Good Bye!")
-    exit()
+    quit_game()
 
 print("A random number has been generated")
 print("Can you guess it in 3 tries? \n")
@@ -41,3 +45,4 @@ for t in range(0, trys):
 
 print("Sorry you did not guess the correct number... :(")
 print(f"The correct answer was {secret}. Better luck next time.")
+quit_game()
