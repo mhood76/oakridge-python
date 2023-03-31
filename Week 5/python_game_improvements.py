abddb8b8914a69e4b3649c629988ff7a3f986357
Mark <37279902+mhood76@users.noreply.github.com>
2023-03-31 13:14:03 -0500

    Added improved version of exercise

--- Week 5/python_game_improvements.py
+++ Week 5/python_game_improvements.py
@@ -1,1 +1,23 @@
+from random import randrange
 
+def quit_game():
+    print("Good Bye!")
+    quit()
+
+'''
+Added a `While` Loop and `If` statement to make sure
+We only accept Y or N as answers.
+'''
+while True:
+    answer = input("Do you want to play? (Y/N) ")
+    if answer in ['Y', 'N']: 
+        break
+    print("I did not understand, Enter Y for Yes or N for No!")
+    
+if answer == 'N':
+    quit_game()
+    
+print("You get 3 tries to guess the random number between 0 and 10")
+
+secret_number = randrange(0, 10, 1)
+print(secret_number)
