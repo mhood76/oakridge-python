b03b5f01388916cc234e88dc0ac1bd09255ed415
Mark <37279902+mhood76@users.noreply.github.com>
2023-03-31 10:56:38 -0500

    Class example

--- Week 5/python_game.py
+++ Week 5/python_game.py
@@ -1,1 +1,15 @@
-#
+from random import randrange
+
+def quit_game():
+    print("Good Bye!")
+    quit()
+
+answer = input("Do you want to play? (Y/N) ")
+
+if answer == "N":
+    quit_game()
+    
+print("You get 3 tries to guess the random number between 0 and 10")
+
+secret_number = randrange(0, 10, 1)
+print(secret_number)
