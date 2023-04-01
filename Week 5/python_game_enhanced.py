7fb163df99a14c4299a7e8e73ad8bbf7e1f10164
Mark Hood <mark@MacBook-Air.local>
2023-04-01 14:00:34 -0500

    Week 6 additions

--- Week 5/python_game_enhanced.py
+++ Week 5/python_game_enhanced.py
@@ -21,3 +21,26 @@ print("You get 3 tries to guess the random number between 0 and 10")
 
 secret_number = randrange(0, 10, 1)
 print(secret_number)
+trys = 3
+
+for t in range(0, trys):
+    guess = int(input("Please guess a number: "))
+    if guess == secret_number:
+        print("You Won!")
+        quit_game()
+        
+    print("That was not correct!")
+    
+print("Sorry you lost the game.")
+print(f"The secret number is {secret_number}")
+quit_game()
+    
+        
+
+
+
+
+
+
+
+
